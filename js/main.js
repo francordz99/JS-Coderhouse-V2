3780fc898560bcbd441a793f167f0cd871bb4700
Franco Rodriguez <francordz32@gmail.com>
2023-02-02 04:54:08 -0300

    Agregado Fetch y Librerias

---
+++ js/main.js
@@ -0,0 +1,57 @@
+const shopCards = document.getElementById("shopCards");
+const viewcart = document.getElementById("viewCart");
+const cartContainer = document.getElementById("cart-container");
+const cartQuantity = document.getElementById("cartQuantity");
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+products.forEach((product) => {
+    let content = document.createElement("div");
+    content.classList.add('col-12', 'col-sm-6', 'col-lg-4');
+    content.innerHTML = `
+        <div class="card">
+        <img src="${product.image}" class="card-img-top" alt="Neumatico">
+        <div class="card-body">
+        <h2 class="card-title">${product.name}</h2>
+        <p class="card-text">${product.price}</p>
+        </div>
+        </div>
+        `;
+
+    shopCards.append(content);
+
+    let buy = document.createElement("button");
+    content.classList.add("btn", "btn-outline-danger");
+    buy.innerText = "Comprar";
+
+    content.append(buy);
+
+    buy.addEventListener("click", () => {
+
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+        if (repeat) {
+            cart.map((prod) => {
+                if (prod.id === product.id) {
+                    prod.quantity++;
+                }
+            });
+        } else {
+            cart.push({
+                id: product.id,
+                image: product.image,
+                name: product.name,
+                price: product.price,
+                quantity: product.quantity,
+            });
+        };
+        console.log(cart);
+        cartCounter();
+        saveLocal();
+    });
+});
+
+const saveLocal = () => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+};
+
+
---
+++ js/viewcart.js
@@ -0,0 +1,110 @@
+
+const cartFunction = () => {
+    cartContainer.innerHTML = "";
+    cartContainer.style.display = "flex";
+    const cartHeader = document.createElement("div");
+    cartHeader.classList = "cartHeader";
+    cartHeader.innerHTML = `
+    <h3 class="cart-title">Tu Carrito<h3>
+    `;
+    cartContainer.append(cartHeader);
+
+    const cartButton = document.createElement("button");
+    cartButton.innerText = "❌";
+    cartButton.className = "cartButton";
+    cartButton.addEventListener("click", () => {
+        cartContainer.style.display = "none";
+    })
+
+    cartHeader.append(cartButton);
+
+    cart.forEach((product) => {
+        let cartContent = document.createElement("div");
+        cartContent.classList = "cartContent";
+        cartContent.innerHTML = `
+        <img src="${product.image}">
+        <h3>${product.name}</h3>
+        <p>${product.price}</p>
+        <span class="substract">-</span>
+        <p>Cantidad : ${product.quantity}</p>
+        <span class="add">+</span>
+        <p>Total : ${product.quantity * product.price}</p>
+        <span class="delete-product">❌</span>
+        `;
+
+        cartContainer.append(cartContent);
+
+        let substract = cartContent.querySelector(".substract")
+        substract.addEventListener("click", () => {
+            if (product.quantity !== 1) {
+                product.quantity--;
+            }
+            saveLocal();
+            cartFunction();
+        });
+
+        let add = cartContent.querySelector(".add")
+        add.addEventListener("click", () => {
+            product.quantity++;
+            saveLocal();
+            cartFunction();
+        });
+
+        let deleteItem = cartContent.querySelector(".delete-product");
+        deleteItem.addEventListener("click", () => {
+            deleteItems(product.id);
+        });
+
+    });
+
+    fetch('https://api.bluelytics.com.ar/v2/latest')
+        .then(response => response.json())
+        .then(json => showExchange(json))
+        .catch(err => console.log('Solicitud fallida', err));
+
+    const showExchange = (json) => {
+        let cotizacion = document.createElement('div');
+        cartContainer.append(cotizacion)
+        cotizacion.innerHTML += `Cotizacion Dolar Blue Hoy: $${json.blue.value_sell}\n <br>`;
+    };
+
+    const finalPrice = cart.reduce((acc, el) => acc + el.price * el.quantity, 0);
+    const finalPriceShown = document.createElement("div")
+    finalPriceShown.className = "total-content"
+    finalPriceShown.innerHTML = `El total a pagar es de: ${finalPrice} Dolares Americanos
+    <button id="checkout">Finalizar Compra</button>`
+    cartContainer.append(finalPriceShown);
+
+    const checkout = document.getElementById("checkout");
+    checkout.addEventListener("click", () => {
+        Swal.fire(
+            'Excelente!',
+            'Tu compra ha sido confirmada y sera despachada a la brevedad',
+            'success',
+        )
+    })
+};
+
+viewcart.addEventListener("click", cartFunction);
+
+const deleteItems = (id) => {
+    const foundId = cart.find((element) => element.id === id);
+    cart = cart.filter((cartId) => {
+        return cartId !== foundId;
+    });
+    cartCounter();
+    saveLocal();
+    cartFunction();
+};
+
+const cartCounter = () => {
+    cartQuantity.style.display = "block";
+
+    const cartLength = cart.length;
+    localStorage.setItem("cartLength", JSON.stringify(cartLength));
+
+    cartQuantity.innerText = JSON.parse(localStorage.getItem("cartLength"));
+};
+
+cartCounter();
+
